3a3d0e374d107cc9a7195293b8a150891b273472
Walid Omar <walidomar291@gmail.com>
2024-05-28 18:39:04 +0300

    fetching employees data to work on it

--- Filtering/Program.cs
+++ Filtering/Program.cs
@@ -1,28 +1,38 @@
-﻿namespace Filtering
+﻿using SharedData;
+using System.Linq;
+
+
+namespace Filtering
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            var numbers = new List<int> { 1,2,3,4,5,6,7,8,9,10};
-            
-            // Query Syntex way
-            var resultQuery = from number in numbers where number < 5 select number;
+            //var numbers = new List<int> { 1,2,3,4,5,6,7,8,9,10};
 
-            foreach (var number in resultQuery)
-            {
-                Console.WriteLine(number);
-            }
+            //// Query Syntex way
+            //var resultQuery = from number in numbers where number < 5 select number;
 
-            Console.WriteLine("========================================");
-            // Method Syntex way
-            var resultMethod = numbers.Where(number => number < 5);
+            //foreach (var number in resultQuery)
+            //{
+            //    Console.WriteLine(number);
+            //}
 
-            foreach (var number in resultQuery)
-            {
-                Console.WriteLine(number);
-            }
+            //Console.WriteLine("========================================");
+            //// Method Syntex way
+            //var resultMethod = numbers.Where(number => number < 5);
 
+            //foreach (var number in resultQuery)
+            //{
+            //    Console.WriteLine(number);
+            //}
+
+
+            var employees = Repository.LoadEmployees();
+
+            var selectedEmps = employees.Where(x => x.Salary > 200000);
+
+            selectedEmps.Print("Employees with salary > 200000");
 
         }
     }
---
+++ SharedData/ExtensionFunctions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedData
+{
+    public static class ExtensionFunctions
+    {
+        public static void Print<T>(this IEnumerable<T> source, string title)
+        {
+            if (source == null)
+                return;
+            Console.WriteLine();
+            Console.WriteLine("┌───────────────────────────────────────────────────────┐");
+            Console.WriteLine($"│   {title.PadRight(52, ' ')}│");
+            Console.WriteLine("└───────────────────────────────────────────────────────┘");
+            Console.WriteLine();
+            foreach (var item in source)
+            {
+                if (typeof(T).IsValueType)
+                    Console.Write($" {item} "); // 1, 2, 3
+                else
+                    Console.WriteLine(item);
+            }
+        }
+    }
+}
