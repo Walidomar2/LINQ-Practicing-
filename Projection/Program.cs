f3946b3f77dfb04f49cb33ae7ed1d8e07e81ada9
Walid Omar <walidomar291@gmail.com>
2024-05-28 22:16:39 +0300

    projection method

--- Projection/Program.cs
+++ Projection/Program.cs
@@ -1,4 +1,6 @@
 ﻿using SharedData;
+using System.Drawing;
+using System.Linq;
 
 namespace Projection
 {
@@ -6,9 +8,59 @@ namespace Projection
     {
         static void Main(string[] args)
         {
-            var employees = Repository.LoadEmployees();
-            employees.Print("All");
+
+            // 1) Select
+            //SelectFunc();
+
+            // 2) Select Many
+            SelectManyFunc();
 
         }
+
+        public static void SelectFunc()
+        {
+            var employees = Repository.LoadEmployees();
+
+            var result = employees.Select(x => new {
+                x.Id,
+                Skills = string.Join(", ", x.Skills)
+            });
+
+
+            result.Print("Id + skills");
+        }
+
+        public static void SelectManyFunc()
+        {
+            var employees = Repository.LoadEmployees();
+
+            // you can remove Distinct
+            var result = employees.SelectMany(x => x.Skills ).Distinct();
+
+            result.Print("skills");
+
+            //Query Syntex
+            //var result2 = from emp in employees
+            //              from skill in emp.Skills
+            //              select skill;
+
+            //result2.Print("skills");
+        }
+
+        public static void ZipFunc()
+        {
+            string[] colorName = { "Red", "Green", "Blue" };
+            string[] colorHEX = { "FF0000", "00FF00", "0000FF", "extra" };
+
+            var colors = colorName.Zip(colorHEX, (name, hex) => $"{name} ({hex})");
+
+            foreach (var color in colors)
+                Console.WriteLine(color);
+        }
+
+
+
+
+
     }
 }
