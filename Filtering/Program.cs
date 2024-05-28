e24535cb516aa01f459677ce04fa9cc2b3d791ca
Walid Omar <walidomar291@gmail.com>
2024-05-28 18:15:54 +0300

    Add project files.

---
+++ Filtering/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Filtering
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new List<int> { 1,2,3,4,5,6,7,8,9,10};
+            
+            // Query Syntex way
+            var resultQuery = from number in numbers where number < 5 select number;
+
+            foreach (var number in resultQuery)
+            {
+                Console.WriteLine(number);
+            }
+
+            Console.WriteLine("========================================");
+            // Method Syntex way
+            var resultMethod = numbers.Where(number => number < 5);
+
+            foreach (var number in resultQuery)
+            {
+                Console.WriteLine(number);
+            }
+
+
+        }
+    }
+}
