570f9efa8a8c6b7a8a62548b2d98eb5fddc96213
Walid Omar <walidomar291@gmail.com>
2024-05-29 18:48:41 +0300

    grouping

---
+++ Grouping/Program.cs
@@ -0,0 +1,51 @@
+﻿using SharedData;
+
+namespace Grouping
+{
+
+    /*
+     * GroupBy:  uses deferred execution
+     *           each iterate makes group again
+     *           IEnumerable<IGrouping<TKey, TSource>>
+     * 
+     * ToLookUp: uses immediate execution
+     *           buffer the result in memory 
+     *           ILookUp<TKey, TSource>
+     */
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GroupByFunc();
+        }
+
+        public static void GroupByFunc()
+        {
+            var employees = Repository.LoadEmployees();
+            var groups = employees.GroupBy(x => x.Department);
+
+            foreach ( var group in groups ) 
+            {
+                group.Print($"Employees In {group.Key} Department");
+            }
+
+            //Query Syntax
+            //var groupsQuery = from emp in employees
+            //                  group emp by emp.Department;
+
+        }
+
+        public static void ToLookUpFunc()
+        {
+            var employees = Repository.LoadEmployees();
+            var groups = employees.ToLookup(x => x.Department);
+
+            foreach (var group in groups)
+            {
+                group.Print($"Employees In {group.Key} Department");
+            }
+
+        }
+    }
+}
---
+++ Quantifiers/Program.cs
@@ -0,0 +1,49 @@
+﻿using SharedData;
+
+namespace Quantifiers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // AnyFunc();
+            AllFunc();
+        }
+
+        public static void AnyFunc()
+        {
+            /*
+             * Any => it just check the items if any item met the condition u spicify or not
+             * it return(true/false) and it stop checking when find the first item that meets the condition
+             */
+            var employees = Repository.LoadEmployees();
+
+            //check if any employess named jac
+            var result = employees.Any(x => x.FirstName.Equals("warner", StringComparison.OrdinalIgnoreCase));
+            Console.WriteLine($"any employess named Warner? {result}");
+
+            var salaryResult = employees.Any(x => x.Salary < 20000);
+            Console.WriteLine($"any employee has salary less than 20000? {salaryResult}");
+        }
+
+        public static void AllFunc()
+        {
+            /*
+            * Any => it just check the items if All items met the condition u spicify or not
+            * it return(true/false) and it stop checking when find the first item that meets the condition
+            */
+
+            var employees = Repository.LoadEmployees();
+
+            var salaryResult = employees.All(x => x.Salary > 20000);
+            Console.WriteLine($"Is All Employees take salary more than 20000? {salaryResult}");
+
+            var skillResult = employees.All(x => x.Skills.Count > 2 );
+            Console.WriteLine($"Is All Employees has Skills more than 2 skills? {skillResult}");
+
+            var skillResult2 = employees.All(x => x.Skills.Any(s => s == "C#"));
+            Console.WriteLine($"Is All Employees has C# Skill? {skillResult2}");
+
+        }
+    }
+}
